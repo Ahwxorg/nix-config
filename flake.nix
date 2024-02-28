9f9ecab0a2918246262f973d726c8dd4dca7db41
Ahwx <ahwx@ahwx.org>
2024-02-28 21:02:55 +0100

    feat(flake.nix): adds

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
