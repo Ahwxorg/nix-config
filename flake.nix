6ad97632e9cb0bfa1aa5733a5660d5461b2ebd71
Ahwx <ahwx@ahwx.org>
2024-04-03 14:31:48 -0700

    feat: add iceshrimp module

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    iceshrimp.url = "git+https://iceshrimp.dev/iceshrimp/packaging";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
